22212fc20720a06255dd7bfc6730562fb3b88d0f
Joshua Montross <joshua.montross@gmail.com>
2013-01-13 14:26:48 -0800

    ads oAuth hack attempts and Readme info about bug

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,15 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  
+
   if Rails.env.production?
     provider :slc, ENV[:SLC_KEY], ENV['SLC_SECRET']
   else
-    provider :slc, ENV['SLC_SANDBOX_KEY'], ENV['SLC_SANDBOX_SECRET'], :setup => lambda{|env| 
-       env['omniauth.strategy'].options[:client_options].site = 'https://api.sandbox.slcedu.org'
+    #{:redirect_uri =>  "http://localhost:3000/auth/slc/callback"},
+    provider :slc, ENV['SLC_SANDBOX_KEY'], ENV['SLC_SANDBOX_SECRET'], :setup => lambda{|env|
+       env['omniauth.strategy'].options[:client_options].site = 'https://api.sandbox.slcedu.org',
+       # env['omniauth.strategy'].options[:redirect_uri].localhost = 'http://localhost:3000/auth/slc/callback'
+       #env['omniauth.params'] = {:redirect_uri => 'http://localhost:3000/auth/slc/callback'}
+       #env['omniauth.params'] = {:redirect_uri => URI.parse("http://localhost:3000/auth/slc/callback")}
+       #{:redirect_uri =>  "http://localhost:3000/auth/slc/callback"}
     }
   end
 
